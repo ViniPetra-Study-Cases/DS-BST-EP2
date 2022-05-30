da1a29a446046bf7052179397b61be5c9ac09053
ViniPetra <viniciuspetratti@hotmail.com>
2022-05-29 22:31:46 -0300

    First comit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # DS-BST-EP2
+O programa foi estruturado de forma bem simples:
+Por tópicos, temos em ordem as funções da BST e da AVL
+Dentro de cada tópico, temos as seguintes funções para cada tipo de árvore:
+
+Definição da estrutura
+Função para criar uma folha
+Função para buscar um valor
+Função para inserir
+Função para saber a altura
+Função para dar free na memória
+--Na AVL ainda temos RotR e RorL
+
+A função main gera valores aleatórios em um vetor do tamanho que o usuário escolher de amostra/folhas e usa estes valores na inserção.
+O tempo é medido e guardado para geração das médias.
+
+A BST é uma das estrutura de dados mais interessantes que já vi pois é intuitiva de aprender mas não necessariamente intuitiva para "surgir com a ideia"
+
+RotL e RotR, "on the other hand", foram (e ainda são) uma pedra no sapato. Sempre tenho necessidade de olhar uma cola para implementação.
+
--- main.c
+++ main.c
@@ -2,147 +2,304 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <time.h>
+#define MAX(a, b)   ((a > b) ? a : b)
 
-typedef struct node{
-    int info;
-    //int h; //altura
-    struct node* esq;
-    struct node* dir;
-}Node;
-typedef Node* Cell;
+//BST simples
+typedef struct nodeBST{
+  int info;
+  int altura;
+  struct nodeBST* esq;
+  struct nodeBST* dir;
+}nodeBST;
+typedef nodeBST* BST;
 
-Cell bstNewNode(int value){
-    Cell node = malloc(sizeof(Cell));
-    node->dir = NULL;
-    node->esq = NULL;
-    node->info = value;
-    return node;
+BST bstNewNode(int value){
+  BST node = malloc(sizeof(nodeBST));
+  node->dir = NULL;
+  node->esq = NULL;
+  node->info = value;
+  node->altura = 1;
+  return node;
 }
 
-Cell bstSearch(Cell root, int target){
-    if(root == NULL || root->info == target){
-        return root;
+BST bstSearch(BST root, int target){
+  if(root == NULL || root->info == target){
+      return root;
+  }
+  else if(root->info > target){
+      return bstSearch(root->esq, target);
+  }
+  else
+  {
+      return bstSearch(root->dir, target);
+  }
+}
+
+BST bstInsert(BST root, int value){
+  if(root == NULL){
+      return bstNewNode(value);
     }
-    else if(target > root->info){
-        return bstSearch(root->dir, target);
+
+    if(root->info == value){
+      return root;
+    }
+
+    if(root->info > value){
+      root->esq = bstInsert(root->esq, value);
+    }
+
+    else {
+      root->dir = bstInsert(root->dir, value);
+  }
+
+  return root;
+}
+
+int bstHeight(BST root){
+  if(root == NULL) {
+    return -1;
+  }
+  else
+  {
+    int esq = bstHeight(root->esq);
+    int dir = bstHeight(root->dir);
+
+    if(esq > dir){
+      return esq + 1;
     }
     else
     {
-        return bstSearch(root->esq, target);
+      return dir + 1;
+    }
+  }
+}
+
+BST min_BST(BST root){
+    while(root->esq){
+        root = root->esq;
+    }
+
+    return root;
+}
+BST max_BST(BST root){
+    while(root->dir){
+        root = root->dir;
+    }
+
+    return root;
+}
+
+void bstInOrder(BST cell){
+  if(cell == NULL){
+      return;
+  }
+  bstInOrder(cell->esq);
+  printf("%d", cell->info);
+  bstInOrder(cell->dir);
+}
+
+void bstPreOrder(BST cell){
+  if(cell == NULL){
+      return;
+  }
+  printf("%d", cell->info);
+  bstPreOrder(cell->esq);
+  bstPreOrder(cell->dir);
+}
+
+void bstPostOrder(BST cell){
+  if(cell == NULL){
+      return;
+  }
+  bstPostOrder(cell->esq);
+  bstPostOrder(cell->dir);
+  printf("%d", cell->info);
+}
+
+void GarbageCollection(BST root){
+    if(root){
+        GarbageCollection(root->esq);
+        GarbageCollection(root->dir);
+        free(root);
     }
 }
 
-Cell bstMin(Cell root) {
-    if (root == NULL || root->esq == NULL) {
-        return root;
-    }
+//BST AVL
+typedef struct AVLnode{
+  int infoAVL;
+  struct AVLnode * esq;
+  struct AVLnode * dir;
+  int AVLHeight;
+}AVLnode;
+
+typedef AVLnode * AVL;
+
+AVL AVLNewNode(int info){
+  AVL newAVL = malloc(sizeof(AVLnode));
+  newAVL->infoAVL = info;
+  newAVL->esq = NULL;
+  newAVL->dir = NULL;
+  newAVL->AVLHeight = 1;
+  return newAVL;
+}
+
+unsigned AVLNodeHeight(AVL node) {
+    if (!node) return 0;
+    return node->AVLHeight;
+}
+
+int AVLHeight(AVL node) {
+    return 1 + MAX(AVLNodeHeight(node->esq), AVLNodeHeight(node->dir));
+}
+
+int AVLBalanceFactor(AVL node){
+  return (AVLNodeHeight(node->esq) - (AVLNodeHeight(node->dir)));
+}
+
+AVL rotL(AVL node) {
+    AVL Aux;
+    Aux = node->dir;
+    node->dir = Aux->esq;
+    Aux->esq = node;
+    node->AVLHeight = AVLHeight(node);
+    Aux->AVLHeight = AVLHeight(Aux);
+    return Aux;
+}
+
+AVL rotR(AVL node) {
+    AVL Aux;
+    Aux = node->esq;
+    node->esq = Aux->dir;
+    Aux->dir = node;
+    node->AVLHeight = AVLHeight(node);
+    Aux->AVLHeight = AVLHeight(Aux);
+    return Aux;
+}
+
+AVL AVLInsert(AVL root, int info) {
+    if (!root) return AVLNewNode(info);
+    else if (root->infoAVL == info) return root;
+    else if (root->infoAVL > info)
+        root->esq = AVLInsert(root->esq, info);
     else
-    {
-        return bstMin(root->esq);
+        root->dir = AVLInsert(root->dir, info);
+
+    root->AVLHeight = AVLHeight(root);
+    int fator = AVLBalanceFactor(root);
+
+    if (fator == +2) {
+        if (AVLBalanceFactor(root->esq) < 0)
+            root->esq = rotL(root->esq);
+        root = rotR(root);
     }
+    else if (fator == -2) {
+        if (AVLBalanceFactor(root->dir) > 0)
+            root->dir = rotR(root->dir);
+        root = rotL(root);
+    }
+    return root;
 }
 
-Cell bstMax(Cell root){
-    if (root == NULL || root->dir == NULL) {
-        return root;
+void libera_AVL(AVL root) {
+    if (root != NULL) {
+        libera_AVL(root->esq);
+        libera_AVL(root->dir);
+        free(root);
     }
-    else
-    {
-        return bstMax(root->dir);
-    }
-}
-
-Cell bstInsert(Cell root, int value){
-    if(root == NULL){
-        return bstNewNode(value);
-    }else if(value > root->info){
-        root->dir = bstInsert(root->dir, value);
-    }else {
-        root->esq = bstInsert(root->esq, value);
-    }
-}
-
-int bstHeight(Cell root){
-    if(root == NULL) {
-        return 0;
-    }
-    else
-    {
-        int esq = bstHeight(root->esq);
-        int dir = bstHeight(root->dir);
-
-        if(esq > dir){
-            return esq + 1;
-        }
-        else
-        {
-            return dir + 1;
-        }
-    }
-}
-
-int bstBalanceFactor(Cell cell){
-    return (bstHeight(cell->esq) - (bstHeight(cell->dir)));
-}
-
-//https://www.geeksforgeeks.org/tree-traversals-inorder-preorder-and-postorder/
-///Algorithm Inorder(tree)
-//1. Traverse the left subtree, i.e., call Inorder(left-subtree)
-//2. Visit the root.
-//3. Traverse the right subtree, i.e., call Inorder(right-subtree)
-void bstInOrder(Cell cell){
-    if(cell == NULL){
-        return;
-    }
-    bstInOrder(cell->esq);
-    printf("%d", cell->info);
-    bstInOrder(cell->dir);
-}
-
-///Algorithm Preorder(tree)
-//1. Visit the root.
-//2. Traverse the left subtree, i.e., call Preorder(left-subtree)
-//3. Traverse the right subtree, i.e., call Preorder(right-subtree)
-void bstPreOrder(Cell cell){
-    if(cell == NULL){
-        return;
-    }
-    printf("%d", cell->info);
-    bstPreOrder(cell->esq);
-    bstPreOrder(cell->dir);
-}
-
-///Algorithm Postorder(tree)
-//1. Traverse the left subtree, i.e., call Postorder(left-subtree)
-//2. Traverse the right subtree, i.e., call Postorder(right-subtree)
-//3. Visit the root.
-void bstPostOrder(Cell cell){
-    if(cell == NULL){
-        return;
-    }
-    bstPostOrder(cell->esq);
-    bstPostOrder(cell->dir);
-    printf("%d", cell->info);
-}
-
-//https://ranger.uta.edu/~alex/courses/3318/lectures/08_search_BST.pdf
-
-Cell bstRotL (Cell cell){
-    Cell x = cell->dir;
-    cell->dir = x->esq;
-    x->esq = cell;
-    return x;
-}
-
-Cell bstRotR (Cell cell){
-    Cell x = cell->esq;
-    cell->esq = x->dir;
-    x->dir = cell;
-    return x;
 }
 
 int main() {
-    printf("Hello, World!\n");
+    int opcao, amostra, elemento;
+    float media_BST = 0, media_AVL = 0, media_geral = 0;
+    double media_tempo_AVL = 0, media_tempo_BST = 0, media_tempo_geral = 0;
+
+    srand(time(NULL));
+    clock_t inicio_BST, final_BST;
+    double tempo_BST;
+    clock_t inicio_AVL, final_AVL;
+    double tempo_AVL;
+
+
+    // LOOP DO PROGRAMA
+    do{
+        printf("\n\n--------------------------------------------------------------------\n");
+        printf("1 - Nova Simulação\n2 - Sair\nDigite sua escolha: ");
+        scanf("%d", &opcao);
+        fflush(stdin);
+
+        switch(opcao){
+        case 1:
+            printf("\nDigite a quantidade de amostras: ");
+            scanf("%d", &amostra);
+            fflush(stdin);
+
+            printf("Digite a quantidade de elementos para cada amostra: ");
+            scanf("%d", &elemento);
+            printf("\n");
+            fflush(stdin);
+
+            //INICIA AS ARVORES
+            BST raiz_BST[amostra];
+            AVL raiz_AVL[amostra];
+            for(int i = 0; i < amostra; i++){
+               raiz_BST[i] = NULL;
+               raiz_AVL[i] = NULL;
+            }
+
+            //CRIA VETOR COM VALORES ALEATÓRIOS
+            int v[amostra][elemento];
+            for(int i = 0; i < amostra; i++){
+                    for(int j = 0; j < elemento; j++){
+                        v[i][j] = 1  + rand() % 99;
+                }
+            }
+
+            //INSERE VALORES ALEATÓRIOS NAS ARVORES
+            for(int i = 0; i < amostra; i++){
+                for(int j = 0; j < elemento; j++){
+
+                    //EXPERIMENTO NA ARVORE BST COMUM
+                    inicio_BST = clock();
+                    raiz_BST[i] = bstInsert(raiz_BST[i], v[i][j]);
+                    final_BST = clock();
+                    tempo_BST = ((double) (final_BST-inicio_BST)/CLOCKS_PER_SEC);
+                    media_tempo_BST += tempo_BST;
+
+                    //EXPERIMENTO NA ARVORE AVL
+                    inicio_AVL = clock();
+                    raiz_AVL[i] = AVLInsert(raiz_AVL[i], v[i][j]);
+                    final_AVL = clock();
+                    tempo_AVL = ((double) (final_AVL-inicio_AVL)/CLOCKS_PER_SEC);
+                    media_tempo_AVL += tempo_AVL;
+                }
+
+                media_geral = ((media_BST  / amostra) + (media_AVL / amostra)) / 2;
+                media_BST += bstHeight(raiz_BST[i]);
+                media_AVL += AVLHeight(raiz_AVL[i]);
+                media_tempo_geral = ((media_tempo_AVL / amostra) + (media_tempo_BST / amostra)) / 2;
+
+            }
+
+            //RETORNO DE TODOS OS TESTES
+            printf("\n");
+            printf("Experimento com A = %d e N = %d\n", amostra, elemento);
+
+            printf("\nAltura média geral: %.2f\n", media_geral);
+            printf("Tempo médio geral de construção: %lf\n\n", media_tempo_geral);
+
+            printf("Altura média BST comum: %.2f\n", media_BST / amostra);
+            printf("Tempo médio de construção BST comum: %lf\n\n", media_tempo_BST / amostra);
+
+            printf("Altura média AVL: %.2f\n", media_AVL / amostra);
+            printf("Tempo médio de construção AVL: %lf\n\n", media_tempo_AVL / amostra);
+            printf("\n");
+            break;
+        }
+
+    }while(opcao != 2);
+
+    printf("Encerrando...");
+
     return 0;
 }
-
